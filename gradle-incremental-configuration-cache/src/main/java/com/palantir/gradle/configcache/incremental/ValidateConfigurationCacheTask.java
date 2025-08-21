63de4fa4d201148c7f0e03c5342c424345168f42
Finlay Williams <finlayw@palantir.com>
2025-08-21 11:34:00 +0100

    this is how todo it

--- gradle-incremental-configuration-cache/src/main/java/com/palantir/gradle/configcache/incremental/ValidateConfigurationCacheTask.java
+++ gradle-incremental-configuration-cache/src/main/java/com/palantir/gradle/configcache/incremental/ValidateConfigurationCacheTask.java
@@ -15,13 +15,16 @@
  */
 package com.palantir.gradle.configcache.incremental;
 
+import com.google.common.collect.ImmutableList;
 import java.io.ByteArrayOutputStream;
 import java.util.Set;
 import javax.inject.Inject;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.ProjectLayout;
+import org.gradle.api.provider.Property;
 import org.gradle.api.provider.SetProperty;
 import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.tooling.GradleConnectionException;
 import org.gradle.tooling.GradleConnector;
@@ -31,6 +34,10 @@ public abstract class ValidateConfigurationCacheTask extends DefaultTask {
 
     @Input
     public abstract SetProperty<String> getAllowedTasks();
+
+    @Input
+    @Optional
+    public abstract Property<String> getInitScript();
 
     @Inject
     protected abstract ProjectLayout getProjectLayout();
@@ -52,11 +59,20 @@ public abstract class ValidateConfigurationCacheTask extends DefaultTask {
         try (ProjectConnection connection = connector.connect()) {
             ByteArrayOutputStream errorStream = new ByteArrayOutputStream();
 
+            ImmutableList.Builder<String> argumentsBuilder = ImmutableList.builder();
+            argumentsBuilder.add("--dry-run", "--configuration-cache");
+
+            // GradleConnector runs builds in a separate process, so we must explicitly pass init scripts.
+            // This is required for Nebula tests, which rely on init scripts for test setup.
+            if (getInitScript().isPresent() && !getInitScript().get().isBlank()) {
+                argumentsBuilder.add("--init-script=" + getInitScript().get());
+            }
+
             try {
                 connection
                         .newBuild()
                         .forTasks(tasks.toArray(new String[0]))
-                        .withArguments("--dry-run", "--configuration-cache")
+                        .withArguments(argumentsBuilder.build())
                         .setStandardError(errorStream)
                         .run();
 
---
+++ gradle-incremental-configuration-cache/src/test/groovy/com/palantir/gradle/configcache/incremental/ValidateConfigurationCacheTaskTest.groovy
@@ -0,0 +1,97 @@
+/*
+ * (c) Copyright 2025 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.palantir.gradle.configcache.incremental
+
+import com.palantir.gradle.plugintesting.ConfigurationCacheSpec
+import org.gradle.testkit.runner.TaskOutcome
+
+class ValidateConfigurationCacheTaskTest extends ConfigurationCacheSpec {
+
+    def setup() {
+        // language=gradle
+        buildFile << '''
+        apply plugin: 'com.palantir.incremental-configuration-cache'
+        apply plugin: 'java-library'
+        
+        tasks.named("validateConfigurationCacheAllowList") {
+            initScript.set(file(".gradle-test-kit/init.gradle").absolutePath)
+        }
+        
+        '''.stripIndent(true)
+    }
+
+    def "validates empty task list"() {
+        given:
+        file("gradle/configuration-cache-allowed-tasks") << ''
+
+        when:
+        def result = runTasksWithConfigurationCache(true, false, 'validateConfigurationCacheAllowList')
+
+        then:
+        result.task(':validateConfigurationCacheAllowList').outcome == TaskOutcome.SUCCESS
+        result.output.contains('No tasks to validate')
+    }
+
+    def "validates compatible tasks successfully"() {
+        given:
+        // We must have validateConfigurationCacheAllowList in the allow list to allow
+        // validateConfigurationCacheAllowList to run with configuration cache
+        file("gradle/configuration-cache-allowed-tasks") << '''
+            :compileJava
+            :processResources
+            :validateConfigurationCacheAllowList
+        '''.stripIndent(true)
+
+        when:
+        def result = runTasksWithConfigurationCacheAndCheck('validateConfigurationCacheAllowList')
+
+        then:
+        result.task(':validateConfigurationCacheAllowList').outcome == TaskOutcome.SUCCESS
+        result.output.contains('Validating configuration cache for 3 tasks')
+        result.output.contains('All 3 tasks passed configuration cache validation')
+    }
+
+    def "fails validation for incompatible tasks"() {
+        given:
+        file("gradle/configuration-cache-allowed-tasks") << '''
+            :problematicTask
+            :validateConfigurationCacheAllowList
+        '''.stripIndent(true)
+
+        buildFile << '''
+        task problematicTask {
+            'echo test'.execute() // Configuration cache problem at configuration time
+        }
+        '''.stripIndent(true)
+
+        when:
+        def result = runTasksAndFail('validateConfigurationCacheAllowList')
+
+        then:
+        result.task(':validateConfigurationCacheAllowList').outcome == TaskOutcome.FAILED
+        result.output.contains('Configuration cache validation failed')
+    }
+
+    def "validation task is hooked into check task"() {
+        given:
+        file("gradle/configuration-cache-allowed-tasks") << ''
+        when:
+        def result = runTasks('check', '--dry-run')
+
+        then:
+        result.output.contains(':validateConfigurationCacheAllowList')
+    }
+}
